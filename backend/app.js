400dd1a9c6d041c7f25af15b3bdaf4d381c0e04a
Irfan <irfan10112001@gmail.com>
2026-07-11 19:39:42 +0700

    Fixing CORS Production

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,10 @@ const app = express();
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://elegant-pavlova-1b1bfa.netlify.app/",
+  );
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
